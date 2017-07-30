e98d247be309da812c99b254f5603cecaf68ec66
Jocelyn Hsu <hsu.joce@gmail.com>
2017-07-29 18:16:56 -0700

    Best effort to add Action Mailer to send welcome and password reset emails

--- server/app/controllers/api/v1/users_controller.rb
+++ server/app/controllers/api/v1/users_controller.rb
@@ -9,6 +9,23 @@ class Api::UsersController < ApplicationController
     render json: users, each_serializer: UserSerializer
   end
 
+  def create
+    @user = User.new(params[:user])
+
+    respond_to do |format|
+      if @user.save
+        # Tell the UserMailer to send a welcome email after save
+        UserMailer.welcome_email(@user).deliver_later
+
+        format.html { redirect_to(@user, notice: 'User was successfully created.') }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def conditions_met
---
+++ server/app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "qb@kittenkaboodleshow.com"
+  layout 'mailer'
+end
