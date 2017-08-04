38770ca0cf4bc26adf70bec1e97bb629968cc3a9
Jocelyn Hsu <hsu.joce@gmail.com>
2017-08-04 12:39:54 -0700

    Remove Bootstrap

--- server/public/client/scripts/components/character/Character.js
+++ server/public/client/scripts/components/character/Character.js
@@ -12,20 +12,6 @@
   function characterCtrl(user) {
     var $ctrl = this;
 
-    this.status = {
-      isopen: false
-    };
-
-    this.toggled = function(open) {
-      $log.log('Dropdown is now: ', open);
-    };
-
-    this.toggleDropdown = function(event) {
-      $event.preventDefault();
-      $event.stopPropagation();
-      this.status.isopen = !this.status.isopen;
-    };
-
   }
 
   angular
