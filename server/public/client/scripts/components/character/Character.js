d165fa395ede30499897334771c1e0bcb4865995
Jocelyn Hsu <hsu.joce@gmail.com>
2017-08-12 18:07:33 -0700

    Add Welcome component, add css for character, nav, bottom nav

--- server/public/client/scripts/components/character/Character.js
+++ server/public/client/scripts/components/character/Character.js
@@ -11,7 +11,6 @@
     $ctrl.showOption = showOption;
     $ctrl.visibleOptions = null;
 
-
     $ctrl.allOptions = {
       gender: [
         {
@@ -19,11 +18,208 @@
         },
         {
           name: 'female'
+        },
+        {
+          name: 'royalty'
         }
       ],
       species: [
         {
-          name: 'human'
+          name: 'Siamese'
+        },
+        {
+          name: 'Persian'
+        },
+        {
+          name: 'Abyssinian'
+        },
+        {
+          name: 'Ragamuffin'
+        },
+        {
+          name: 'Main Coon'
+        },
+        {
+          name: 'Ragdoll'
+        },
+        {
+          name: 'American Shorthair'
+        },
+        {
+          name: 'Tonkinese'
+        },
+        {
+          name: 'Russian Blue'
+        },
+        {
+          name: 'Bengal'
+        },
+        {
+          name: 'Scottish Fold'
+        },
+        {
+          name: 'British Shorthair'
+        },
+        {
+          name: 'Siberian'
+        },
+        {
+          name: 'Exotic'
+        },
+        {
+          name: 'Himalayan'
+        }
+      ],
+      eye_color: [
+        {
+          name: 'yellow'
+        },
+        {
+          name: 'green'
+        },
+        {
+          name: 'blue'
+        },
+        {
+          name: 'black'
+        },
+        {
+          name: 'brown'
+        },
+        {
+          name: 'orange'
+        }
+      ],
+      eye_type: [
+        {
+          name: 'normal/smart'
+        },
+        {
+          name: 'walleyed'
+        },
+        {
+          name: 'crosseyed'
+        },
+        {
+          name: 'evil/slitted'
+        }
+      ],
+      nose_color: [
+        {
+          name: 'grey'
+        },
+        {
+          name: 'red'
+        },
+        {
+          name: 'pink'
+        },
+        {
+          name: 'copper'
+        },
+        {
+          name: 'black'
+        }
+      ],
+      kissiepie_mouth_nose: [
+        {
+          name: 'rest'
+        },
+        {
+          name: 'smile'
+        },
+        {
+          name: 'yeah! (open)'
+        },
+        {
+          name: 'smile-cry'
+        },
+        {
+          name: "m'lady/smirk"
+        },
+        {
+          name: 'runs out, tongues out'
+        },
+        {
+          name: 'o-face'
+        },
+        {
+          name: 'jowls'
+        }
+      ],
+      body_type: [
+        {
+          name: 'biskit'
+        },
+        {
+          name: 'normal'
+        },
+        {
+          name: 'slim'
+        },
+        {
+          name: 'tall'
+        },
+        {
+          name: 'roundy'
+        },
+        {
+          name: 'bigboi'
+        },
+      ],
+      outfits: [
+        {
+          name: 'cowboy'
+        },
+        {
+          name: 'business suit'
+        },
+        {
+          name: 'dunce cap'
+        },
+        {
+          name: 'maid'
+        },
+        {
+          name: 'big big muscles'
+        },
+        {
+          name: 'dress'
+        },
+        {
+          name: 'jumpers'
+        },
+        {
+          name: 'mariachi'
+        },
+        {
+          name: 'pirate'
+        },
+        {
+          name: 'ninja'
+        }
+      ],
+      items: [
+        {
+          name: 'briefcase'
+        },
+        {
+          name: 'ID'
+        },
+        {
+          name: 'ice cream cone'
+        },
+        {
+          name: 'slingshot'
+        },
+        {
+          name: 'pencil'
+        },
+        {
+          name: 'ball'
+        },
+        {
+          name: 'sign'
         }
       ]
     }
