7b719776760b07ce2572e769b14c6b93856b726f
Shannon Bertucci <hello@shannon.io>
2017-09-03 18:24:44 -0600

    Fixes first render of kitten

--- server/public/client/scripts/services/character-creator.js
+++ server/public/client/scripts/services/character-creator.js
@@ -7,65 +7,65 @@ function characterCreator($rootScope) {
       body_type: [
         {
           name: 'norm',
-          src: 'kk_kmchar2_norm_'+(charInProgress.species ? charInProgress.species.name :DEFAULT_SPECIES)+'.png'
+          src: 'kk_kmchar2_norm_'+(charInProgress.species ? charInProgress.species.name : 'orange')+'.png'
         },
         {
           name: 'slim',
-          src: 'kk_kmchar2_slim_'+(charInProgress.species ? charInProgress.species.name :DEFAULT_SPECIES)+'.png'
+          src: 'kk_kmchar2_slim_'+(charInProgress.species ? charInProgress.species.name : 'orange')+'.png'
         }
       ],
     species: [
       {
         name: 'orange',
-        src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : DEFAULT_BODY)+'_orange.png'
+        src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : 'norm')+'_orange.png'
       },
       {
         name: 'gray',
-        src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : DEFAULT_BODY)+'_gray.png'
+        src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : 'norm')+'_gray.png'
       }
     ],
     eye_type: [
       {
         name: 'round',
-        src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : DEFAULT_BODY)+'_round_'+(charInProgress.eye_color ? charInProgress.eye_color.name : DEFAULT_EYE_COLOR)+'.png'
+        src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : 'norm')+'_round_'+(charInProgress.eye_color ? charInProgress.eye_color.name : 'blue')+'.png'
       },
       {
         name: 'slit',
-        src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : DEFAULT_BODY)+'_slit_'+(charInProgress.eye_color ? charInProgress.eye_color.name : DEFAULT_EYE_COLOR)+'.png'
+        src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : 'norm')+'_slit_'+(charInProgress.eye_color ? charInProgress.eye_color.name : 'blue')+'.png'
       }
     ],
     eye_color: [
       {
         name: 'green',
-        src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : DEFAULT_BODY)+'_'+(charInProgress.eye_type ? charInProgress.eye_type.name : DEFAULT_EYE_TYPE)+'_green.png'
+        src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : 'norm')+'_'+(charInProgress.eye_type ? charInProgress.eye_type.name : 'round')+'_green.png'
       },
       {
         name: 'blue',
-        src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : DEFAULT_BODY)+'_'+(charInProgress.eye_type ? charInProgress.eye_type.name : DEFAULT_EYE_TYPE)+'_blue.png'
+        src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : 'norm')+'_'+(charInProgress.eye_type ? charInProgress.eye_type.name : 'round')+'_blue.png'
       },
       {
         name: 'brown',
-        src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : DEFAULT_BODY)+'_'+(charInProgress.eye_type ? charInProgress.eye_type.name : DEFAULT_EYE_TYPE)+'_brown.png'
+        src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : 'norm')+'_'+(charInProgress.eye_type ? charInProgress.eye_type.name : 'round')+'_brown.png'
       }
     ],
     kissiepie: [
       {
         name: 'agape',
-        src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : DEFAULT_BODY)+'_agape_'+(charInProgress.nose_color ? charInProgress.nose_color.name : DEFAULT_NOSE_COLOR)+'.png'
+        src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : 'norm')+'_agape_'+(charInProgress.nose_color ? charInProgress.nose_color.name : 'black')+'.png'
       },
       {
         name: 'tongue',
-        src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : DEFAULT_BODY)+'_tongue_'+(charInProgress.nose_color ? charInProgress.nose_color.name : DEFAULT_NOSE_COLOR)+'.png'
+        src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : 'norm')+'_tongue_'+(charInProgress.nose_color ? charInProgress.nose_color.name : 'black')+'.png'
       }
     ],
     nose_color: [
       {
         name: 'black',
-        src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : DEFAULT_BODY)+'_'+(charInProgress.kissiepie ? charInProgress.kissiepie.name : DEFAULT_KISSIEPIE)+'_black.png'
+        src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : 'norm')+'_'+(charInProgress.kissiepie ? charInProgress.kissiepie.name : 'agape')+'_black.png'
       },
       {
         name: 'pink',
-        src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : DEFAULT_BODY)+'_'+(charInProgress.kissiepie ? charInProgress.kissiepie.name : DEFAULT_KISSIEPIE)+'_pink.png'
+        src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : 'norm')+'_'+(charInProgress.kissiepie ? charInProgress.kissiepie.name : 'agape')+'_pink.png'
       }
     ]
   };
@@ -93,7 +93,6 @@ function characterCreator($rootScope) {
 
   function updateChar(character) {
     charInProgress = _.extend(charInProgress, character);
-    formatEyeSrc();
     $rootScope.$broadcast('character.update');
     console.log(charInProgress)
     return current();
@@ -101,15 +100,16 @@ function characterCreator($rootScope) {
 
   function formatEyeSrc() {
     charInProgress.eye_src = 'kk_kmchar2_' +
-    (charInProgress.body_type ? charInProgress.body_type.name : DEFAULT_BODY) +
+    (charInProgress.body_type ? charInProgress.body_type.name : 'norm') +
     '_' +
-    (charInProgress.eye_type ? charInProgress.eye_type.name : DEFAULT_EYE_TYPE) +
+    (charInProgress.eye_type ? charInProgress.eye_type.name : 'round') +
     '_' +
-    (charInProgress.eye_color ? charInProgress.eye_color.name : DEFAULT_EYE_COLOR) +
+    (charInProgress.eye_color ? charInProgress.eye_color.name : 'blue') +
     '.png';
   }
 
   function current() {
+    formatEyeSrc();
     return charInProgress;
   }
 
