bfc670af2ac9c82cf05640962ac290c30930fc85
Jocelyn Hsu <hsu.joce@gmail.com>
2017-09-15 13:09:32 -0700

    Add store option component, best effort to build out scaffold for store modals

--- server/public/client/scripts/components/store/Store.js
+++ server/public/client/scripts/components/store/Store.js
@@ -10,6 +10,71 @@
   function storeCtrl() {
     var $ctrl = this;
 
+    $ctrl.allOptions = {
+      outfits: [
+        {
+          name: 'cloak'
+        },
+        {
+          name: 'trenchcoat'
+        }
+      ],
+      left_hand: [
+        {
+          name: 'briefcase'
+        },
+        {
+          name: 'ID'
+        },
+        {
+          name: 'ice cream cone'
+        },
+        {
+          name: 'slingshot'
+        },
+        {
+          name: 'pencil'
+        },
+        {
+          name: 'ball'
+        },
+        {
+          name: 'sign'
+        }
+      ],
+      right_hand: [
+        {
+          name: 'briefcase'
+        },
+        {
+          name: 'ID'
+        },
+        {
+          name: 'ice cream cone'
+        },
+        {
+          name: 'slingshot'
+        },
+        {
+          name: 'pencil'
+        },
+        {
+          name: 'ball'
+        },
+        {
+          name: 'sign'
+        }
+      ],
+      special_item: [
+        {
+          name: 'brown boots'
+        },
+        {
+          name: 'red boots'
+        }
+      ]
+    }
+
   }
 
   angular
