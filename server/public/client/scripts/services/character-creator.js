ee76fb93bdb5622d2f0311add82c185d2bf86c3a
Shannon Bertucci <hello@shannon.io>
2017-09-24 18:40:01 -0600

    Updates to char service + elite service

--- server/public/client/scripts/services/character-creator.js
+++ server/public/client/scripts/services/character-creator.js
@@ -5,71 +5,32 @@ function characterCreator($rootScope) {
   var characterOpts = function() {
     return {
       body_type: [
-        {
-          name: 'norm',
-          // src: 'kk_kmchar2_norm_'+(charInProgress.species ? charInProgress.species.name : 'orange')+'.png'
-        },
-        {
-          name: 'slim',
-          // src: 'kk_kmchar2_slim_'+(charInProgress.species ? charInProgress.species.name : 'orange')+'.png'
-        }
+        {name: 'norm'},
+        {name: 'slim'}
       ],
-    species: [
-      {
-        name: 'orange',
-        // src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : 'norm')+'_orange.png'
-      },
-      {
-        name: 'gray',
-        // src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : 'norm')+'_gray.png'
-      }
-    ],
-    eye_type: [
-      {
-        name: 'round',
-        // src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : 'norm')+'_round_'+(charInProgress.eye_color ? charInProgress.eye_color.name : 'blue')+'.png'
-      },
-      {
-        name: 'slit',
-        // src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : 'norm')+'_slit_'+(charInProgress.eye_color ? charInProgress.eye_color.name : 'blue')+'.png'
-      }
-    ],
-    eye_color: [
-      {
-        name: 'green',
-        // src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : 'norm')+'_'+(charInProgress.eye_type ? charInProgress.eye_type.name : 'round')+'_green.png'
-      },
-      {
-        name: 'blue',
-        // src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : 'norm')+'_'+(charInProgress.eye_type ? charInProgress.eye_type.name : 'round')+'_blue.png'
-      },
-      {
-        name: 'brown',
-        // src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : 'norm')+'_'+(charInProgress.eye_type ? charInProgress.eye_type.name : 'round')+'_brown.png'
-      }
-    ],
-    kissiepie: [
-      {
-        name: 'agape',
-        // src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : 'norm')+'_agape_'+(charInProgress.nose_color ? charInProgress.nose_color.name : 'black')+'.png'
-      },
-      {
-        name: 'tongue',
-        // src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : 'norm')+'_tongue_'+(charInProgress.nose_color ? charInProgress.nose_color.name : 'black')+'.png'
-      }
-    ],
-    nose_color: [
-      {
-        name: 'black',
-        // src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : 'norm')+'_'+(charInProgress.kissiepie ? charInProgress.kissiepie.name : 'agape')+'_black.png'
-      },
-      {
-        name: 'pink',
-        // src: 'kk_kmchar2_'+(charInProgress.body_type ? charInProgress.body_type.name : 'norm')+'_'+(charInProgress.kissiepie ? charInProgress.kissiepie.name : 'agape')+'_pink.png'
-      }
-    ]
+      species: [
+        {name: 'orange'},
+        {name: 'gray'}
+      ],
+      eye_type: [
+        {name: 'round'},
+        {name: 'slit'}
+      ],
+      eye_color: [
+        {name: 'green'},
+        {name: 'blue'},
+        {name: 'brown'}
+      ],
+      kissiepie: [
+        {name: 'agape'},
+        {name: 'tongue'}
+      ],
+      nose_color: [
+        {name: 'black'},
+        {name: 'pink'}
+      ]
+    };
   };
-};
 
   var DEFAULT_BODY = characterOpts().body_type[0];
   var DEFAULT_SPECIES = characterOpts().species[0];
@@ -84,7 +45,7 @@ function characterCreator($rootScope) {
     eye_type: DEFAULT_EYE_TYPE,
     eye_color: DEFAULT_EYE_COLOR,
     kissiepie: DEFAULT_KISSIEPIE,
-    nose_color: DEFAULT_NOSE_COLOR,
+    nose_color: DEFAULT_NOSE_COLOR
   };
 
   service.updateChar = updateChar;
@@ -98,48 +59,13 @@ function characterCreator($rootScope) {
     return current();
   }
 
-  // function formatEyeSrc() {
-  //   charInProgress.eye_src = 'kk_kmchar2_' +
-  //   (charInProgress.body_type ? charInProgress.body_type.name : 'norm') +
-  //   '_' +
-  //   (charInProgress.eye_type ? charInProgress.eye_type.name : 'round') +
-  //   '_' +
-  //   (charInProgress.eye_color ? charInProgress.eye_color.name : 'blue') +
-  //   '.png';
-  // }
-
   function formatOptionSrc() {
-    _.map(characterOpts(), function(optionsList, characterAttr) {
-      console.log(characterAttr, optionsList);
-      if (characterAttr === ('body_type' || 'species')) {
-        charInProgress[characterAttr].src = 'kk_kmchar2_' +
-        (charInProgress.body_type ? charInProgress.body_type.name : 'norm') +
-        '_' +
-        (charInProgress.species ? charInProgress.species.name : 'orange') +
-        '.png';
-      } else if (characterAttr === ('eye_type' || 'eye_color')) {
-        charInProgress[characterAttr].src = 'kk_kmchar2_' +
-        (charInProgress.body_type ? charInProgress.body_type.name : 'norm') +
-        '_' +
-        (charInProgress.eye_type ? charInProgress.eye_type.name : 'round') +
-        '_' +
-        (charInProgress.eye_color ? charInProgress.eye_color.name : 'green') +
-        '.png';
-      } else if (characterAttr === ('kissiepie' || 'nose_color')) {
-        charInProgress[characterAttr].src = 'kk_kmchar2_' +
-        (charInProgress.body_type ? charInProgress.body_type.name : 'norm') +
-        '_' +
-        (charInProgress.kissiepie ? charInProgress.kissiepie.name : 'agape') +
-        '_' +
-        (charInProgress.nose_color ? charInProgress.nose_color.name : 'black') +
-        '.png';
-      }
-    });
-    console.log(charInProgress);
+    charInProgress.body_type.src = 'assets/kitten_parts/bodies/'+charInProgress.body_type.name+'/'+charInProgress.species.name+'.png';
+    charInProgress.eye_type.src = 'assets/kitten_parts/eyes/'+charInProgress.eye_color.name+'/'+charInProgress.body_type.name+'_'+charInProgress.eye_type.name+'.png';
+    charInProgress.kissiepie.src = 'assets/kitten_parts/kissiepie/'+charInProgress.kissiepie.name+'/'+charInProgress.body_type.name+'_'+charInProgress.nose_color.name+'.png';
   }
 
   function current() {
-    // formatEyeSrc();
     formatOptionSrc();
     return charInProgress;
   }
