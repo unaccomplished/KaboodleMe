a2b40439d5dc38dcc14b76684446e3ff99d7c7ab
Jocelyn Hsu <hsu.joce@gmail.com>
2017-10-15 18:14:14 -0700

    Update in progress for store controller and spec with mentor

--- server/app/controllers/api/v1/store_controller.rb
+++ server/app/controllers/api/v1/store_controller.rb
@@ -1,16 +1,20 @@
 class Api::V1::StoreController < ApplicationController
   def buy
-    @character = Character.find(user_id: current_user)
+    # @character = Character.where(user: current_user)
     @elite_item = EliteItem.find(params[:id])
-    @character.elite_items << @elite_item
-    render json: elite_item, serializer: EliteItemsSerializer
+    current_user.elite_items << @elite_item
+    render json: {
+      elite_items: current_user.elite_items
+      }, serializer: EliteItemsSerializer
   end
 
   def sell
-    @character = Character.find(user_id: current_user)
+    # @character = Character.where(user: current_user)
     @elite_item = EliteItem.find(params[:id])
-    @character.elite_items.delete(@elite_item)
-    render json: elite_item, serlializer: EliteItemsSerializer
+    current_user.elite_items.delete(@elite_item) #Maybe use destory
+    render json:{
+      elite_items: current_user.elite_items
+      }, serlializer: EliteItemsSerializer
   end
 
 end
