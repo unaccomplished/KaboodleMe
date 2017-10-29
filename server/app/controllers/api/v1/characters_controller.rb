9f3a010953c184e59dde08e4294f424d03451472
Jocelyn Hsu <hsu.joce@gmail.com>
2017-10-28 19:01:00 -0700

    Best effort fix controllers and tests

--- server/app/controllers/api/v1/characters_controller.rb
+++ server/app/controllers/api/v1/characters_controller.rb
@@ -1,21 +1,35 @@
 class Api::V1::CharactersController < ApplicationController
   def show
-    character = Character.where(user: current_user ).first
+    character = Character.where(user: current_user).first
     render json: character, serializer: CharacterSerializer, status: 200
   end
 
   def update
-    p Character.all
     character = Character.where(user: current_user).first
-    if character.update_attributes(character_params)
-      render json: character, status: 200
-    else
-      render json: {error: "Character update failed", status: 400}, status: 400
+    trait = Trait.find(params[:id])
+    elite_item = EliteItem.find(params[:id])
+
+    params.each do |key, value|
+      if trait
+        character.traits << trait
+      elsif elite_item
+        character.elite_items << elite_item
+      else
+        character.update_attributes(character_params)
+      end
     end
+    render json: {
+      character: character.traits
+    }, serializer: CharacterSerializer
+    # if character.update_attributes(character_params)
+    #   render json: character, status: 200
+    # else
+    #   render json: {error: "Character update failed", status: 400}, status: 400
+    # end
   end
 
   private
   def character_params
-    params.permit(:name, :trait_id, :elite_item_id)
+    params.permit(:name)
   end
 end
