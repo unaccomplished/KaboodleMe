183a5722d5262629c8b8e9b3c9e5b0fbe2c59e12
Jocelyn Hsu <hsu.joce@gmail.com>
2017-11-26 18:22:44 -0800

    WIP Character Serializer after mentor meeting

--- server/app/serializers/character_serializer.rb
+++ server/app/serializers/character_serializer.rb
@@ -1,9 +1,12 @@
 class CharacterSerializer < ActiveModel::Serializer
   attributes :id, :name, :traits, :elite_items, :sources
 
+  has_many :traits
+
   def sources
-    newTraits = traits.reduce do |result, t|
+    newTraits = object.traits.reduce({}) do |result, t|
       result[t.trait_type] = t.name
+      result
     end
 
     # {
@@ -12,9 +15,13 @@ class CharacterSerializer < ActiveModel::Serializer
     # }
 
     char = Hash.new
-    char["body_type"] = 'assets/kitten_parts/bodies/'+newTraits.body_type.name+'/'+newTraits.species.name+'.png';
-    char["eye_type"] = 'assets/kitten_parts/eyes/'+newTraits.eye_color.name+'/'+newTraits.body_type.name+'_'+newTraits.eye_type.name+'.png';
-    char["kissiepie"] = 'assets/kitten_parts/kissiepie/'+newTraits.kissiepie.name+'/'+newTraits.body_type.name+'_'+newTraits.nose_color.name+'.png';
+    p newTraits
+    body_type=newTraits[:body][:name]
+    species=newTraits.species.name
+    string = 'assets/kitten_parts/bodies/'+body_type+'/'+species+'.png'
+    char[:body_type] = string
+    # char["eye_type"] = 'assets/kitten_parts/eyes/'+newTraits.eye_color.name+'/'+newTraits.body_type.name+'_'+newTraits.eye_type.name+'.png';
+    # char["kissiepie"] = 'assets/kitten_parts/kissiepie/'+newTraits.kissiepie.name+'/'+newTraits.body_type.name+'_'+newTraits.nose_color.name+'.png';
     char
   end
 
